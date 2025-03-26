dd61e96fa9687e6dcba39490a39f7d2b90368f8b
Narges-Jamshidi <iamsabwina@gmail.com>
2025-03-26 19:46:47 +0330

    Completing the algorithm for finding the largest product in a contiguous subarray.

--- HW/Maximum-Product.py
+++ HW/Maximum-Product.py
@@ -9,11 +9,34 @@ def maximum_product(arry):
     right_max = maximum_product(right_arry)
     left_max = maximum_product(left_arry)
 
-    return max(right_max, left_max)
+    mid_max = maximum_mid(arry)
+
+    return max(right_max, left_max, mid_max)
 
 
 def maximum_mid(arry):
     mid = len(arry) // 2
 
+    left_min = 100000
+    right_min = 100000
+    right_max = -100000
+    left_max = -100000
+    mult = 1
+    for i in range(mid, len(arry)):
+        mult = mult * arry[i]
+        right_min = min(right_min, mult)
+        right_max = max(right_max, mult)
+
+
+    mult = 1
+    for i in range(mid-1, -1, -1):
+        mult = mult * arry[i]
+        left_min = min(left_min, mult)
+        left_max = max(left_max, mult)
+
+
+    return max((right_max * left_max), (left_min * right_min))
+
 
 arry = list(map(int, input().split()))
+print(maximum_product(arry))
